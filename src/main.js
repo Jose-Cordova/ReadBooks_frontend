00e62716e767c29e893be7e30ac4ffa95e66f219
José Córdova <josecordoba19@gmail.com>
2026-03-01 20:46:33 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+
+// 1. IMPORTAR ESTILOS DE Tailwind
+import './assets/main.css' 
+
+// 2. ESTILOS DE PRIMEVUE
+import 'primevue/resources/themes/lara-dark-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue)
+app.mount('#app')
