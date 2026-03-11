b6d314a9b4bd1b99543bc3fe0931e06de7aa5b34
Adonay navas <adonaynavas5512@gmail.com>
2026-03-10 18:18:31 -0600

    navbar completo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="bg-gray-200 min-h-screen flex flex-col">
+    <NavBar />
     <HeroCatalogo />
     <SearchBar />
     <LibroGrid />
+
   </div>
 </template>
 
@@ -10,4 +12,7 @@
   import HeroCatalogo from "@/components/home/HeroCatalogo.vue";
   import SearchBar from "@/components/home/SearchBar.vue";
   import LibroGrid from "@/components/home/LibroGrid.vue";
+  import NavBar from '@/components/layouts/Navbar.vue'
+
+
 </script>
